8b1c0c54a54aea8bd7ac5c78bfe7230b70d267c6
Madina <madina-khat@yandex.ru>
2022-11-09 11:09:27 +0300

    1

---
+++ src/Employee.java
@@ -0,0 +1,57 @@
+public class Employee {
+    String firstName;
+    String middleName;
+    String lastName;
+    String fullName;
+    int department;
+    float salary;
+    static int counter;
+    int id;
+
+    public Employee(String lastName, String firstName, String middleName, int department, float salary) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.fullName = lastName + " " + firstName + " " + middleName;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++counter;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
